4a2872d990810a7d42c98626294b9b96523b2201
Onur Mehemmed <onurmamedov1@gmail.com>
2025-06-01 19:17:09 +0400

    Admin Activity Design is ready

--- TourManagementSystem/obj/Debug/net8.0/TourManagementSystem.AssemblyInfo.cs
+++ TourManagementSystem/obj/Debug/net8.0/TourManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a64eb3fc160ccb8219afee5888bf56b82ce2f216")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f11060f306c5a8ca802fb4a1face804552b1fbda")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
